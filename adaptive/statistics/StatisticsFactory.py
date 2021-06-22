1c215acff64be450e5357eae8528449d59820667
ofriol <ofriol15@gmail.com>
2021-06-22 14:15:27 +0300

    StatisticsFactory - Update be aware to multi pattern

--- adaptive/statistics/StatisticsFactory.py
+++ adaptive/statistics/StatisticsFactory.py
@@ -15,7 +15,6 @@ class StatisticsFactory:
     def create_statistics(patterns: List[Pattern], stat_type: StatisticsTypes, statistics_time_window: timedelta):
 
         predefined_statistics = StatisticsFactory.__extract_statistics(patterns, stat_type)
-        StatisticsFactory.__update_patterns(patterns, stat_type, predefined_statistics)
 
         if stat_type == StatisticsTypes.ARRIVAL_RATES:
             return ArrivalRatesStatistics(statistics_time_window, patterns, predefined_statistics)
@@ -26,24 +25,13 @@ class StatisticsFactory:
     @staticmethod
     def __extract_statistics(patterns, stat_type):
         items_to_item_map = {}
-        # for pattern in patterns:
-        #     if pattern.statistics and stat_type in pattern.statistics:
-        #
-        #         for item, statistics in pattern.statistics[stat_type].items():
-        #             if item not in items_to_item_map:
-        #                 items_to_item_map[item] = statistics
+        for pattern in patterns:
+            if pattern.statistics and stat_type in pattern.statistics:
+                pattern.prior_statistics_exist = True
+
+                for item, statistics in pattern.statistics[stat_type].items():
+                    if item not in items_to_item_map:
+                        items_to_item_map[item] = statistics
 
         return items_to_item_map
 
-    @staticmethod
-    def __update_patterns(patterns, stat_type, items_to_item_map):
-        """
-        after extract from every pattern his pre defines statistics, update all patterns if they get
-        predefined statistics from other pattern. For example, 2 pattern with 'a' type and just one of them contain
-        statistics for this arrival rates type, so we wand updata the other pattern to know.
-        """
-        for pattern in patterns:
-            for item in items_to_item_map:
-                if item in pattern.statistics[stat_type].keys():
-                    pattern.prior_statistics_exist = True
-                    continue
