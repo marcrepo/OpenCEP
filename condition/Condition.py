fcb80e795321914de44720911a37bff2e3883110
ofriol <ofriol15@gmail.com>
2021-06-22 14:31:17 +0300

    Condition - Add get_statistics_collector function.

--- condition/Condition.py
+++ condition/Condition.py
@@ -132,6 +132,9 @@ class AtomicCondition(Condition, ABC):
         """
         self._statistics_collector = statistics_collector
 
+    def get_statistics_collector(self):
+        return self._statistics_collector
+
 
 class TrueCondition(AtomicCondition):
     """
