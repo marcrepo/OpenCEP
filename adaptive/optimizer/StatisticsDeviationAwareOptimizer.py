655dcd05817d72aefea9b5ff481610241fe0f266
ofriol <ofriol15@gmail.com>
2021-06-28 11:36:53 +0300

    StatisticsDeviationAwareOptimizer - should_optimize - Fix bug

--- adaptive/optimizer/StatisticsDeviationAwareOptimizer.py
+++ adaptive/optimizer/StatisticsDeviationAwareOptimizer.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from adaptive.optimizer.Optimizer import Optimizer
+from adaptive.statistics.StatisticsCollector import StatisticsCollector
 from base.Pattern import Pattern
 from plan.TreePlanBuilder import TreePlanBuilder
 
@@ -11,7 +12,7 @@ class StatisticsDeviationAwareOptimizer(Optimizer):
     """
 
     def __init__(self, patterns: List[Pattern], tree_plan_builder: TreePlanBuilder, is_adaptivity_enabled: bool,
-                 type_to_deviation_aware_functions_map: dict, statistics_collector):
+                 type_to_deviation_aware_functions_map: dict, statistics_collector: StatisticsCollector):
         super().__init__(patterns, tree_plan_builder, is_adaptivity_enabled, statistics_collector)
         self._prev_statistics = {pattern: statistics_collector.get_specific_statistics(pattern) for pattern in patterns}
         self.__type_to_deviation_aware_tester_map = type_to_deviation_aware_functions_map
@@ -23,12 +24,12 @@ class StatisticsDeviationAwareOptimizer(Optimizer):
             return new_tree_plan
         return None
 
-    def _should_optimize(self, new_statistics: dict, pattern):
+    def _should_optimize(self, new_statistics: dict, pattern: Pattern):
         prev_stats = self._prev_statistics[pattern]
         for new_stats_type, new_stats in new_statistics.items():
             if self.__type_to_deviation_aware_tester_map[new_stats_type].is_deviated_by_t(new_stats,
                                                                                           prev_stats[new_stats_type]):
-                self._prev_statistics[pattern] = prev_stats
+                self._prev_statistics[pattern] = new_statistics
                 return True
         return False
 
