8bed7d946f4f2cffd097de4be651921e32e3d6fd
ofriol <ofriol15@gmail.com>
2021-06-29 16:17:42 +0300

    OptimizerTests - Rewrite optimizer tests

--- test/OptimizerTests.py
+++ test/OptimizerTests.py
@@ -1,13 +1,16 @@
 from datetime import timedelta
 from typing import Dict
+
+from adaptive.statistics.StatisticsCollectorFactory import StatisticsCollectorFactory
 from adaptive.statistics.StatisticsTypes import StatisticsTypes
 from base.Pattern import Pattern
 from base.PatternStructure import SeqOperator, PrimitiveEventStructure
 from condition.Condition import SimpleCondition, Variable
-from adaptive.optimizer.Optimizer import Optimizer
-from adaptive.optimizer.OptimizerFactory import OptimizerFactory
+from adaptive.optimizer.OptimizerFactory import OptimizerFactory, OptimizerParameters
 from test.EvalTestsDefaults import DEFAULT_TESTING_GREEDY_INVARIANT_OPTIMIZER_SETTINGS, \
-    DEFAULT_TESTING_ZSTREAM_INVARIANT_OPTIMIZER_SETTINGS
+    DEFAULT_TESTING_ZSTREAM_INVARIANT_OPTIMIZER_SETTINGS, \
+    DEFAULT_TESTING_STATISTICS_COLLECTOR_SELECTIVITY_AND_ARRIVAL_RATES_STATISTICS
+from unittest.mock import MagicMock
 
 
 def get_pattern_test():
@@ -31,7 +34,6 @@ def greedyInvariantOptimizerTreeChangeFailTest_1():
     The optimizer will say that there is no need to re-optimize
     """
     optimizer_parameters = DEFAULT_TESTING_GREEDY_INVARIANT_OPTIMIZER_SETTINGS
-    optimizer = OptimizerFactory.build_optimizer(optimizer_parameters)
 
     old_arrival_rates = [1, 5, 8, 2]
     old_selectivity_matrix = [[1.0, 0.9457796098355941, 1.0, 1.0], [0.9457796098355941, 1.0, 0.15989723367389616, 1.0],
@@ -45,7 +47,7 @@ def greedyInvariantOptimizerTreeChangeFailTest_1():
     new_statistics = {StatisticsTypes.ARRIVAL_RATES: new_arrival_rates,
                       StatisticsTypes.SELECTIVITY_MATRIX: new_selectivity_matrix}
 
-    is_changed = optimizer_test(old_statistics, new_statistics, optimizer)
+    is_changed = optimizer_test(old_statistics, new_statistics, optimizer_parameters)
     result = "Succeeded" if not is_changed else "Failed"
     print(f"Test greedyInvariantOptimizerTreeChangeFailTest_1 result: {result}")
 
@@ -58,7 +60,6 @@ def greedyInvariantOptimizerTreeChangeFailTest_2():
     The optimizer will say that there is no need to re-optimize
     """
     optimizer_parameters = DEFAULT_TESTING_GREEDY_INVARIANT_OPTIMIZER_SETTINGS
-    optimizer = OptimizerFactory.build_optimizer(optimizer_parameters)
 
     old_arrival_rates = [1, 5, 8, 2]
     old_selectivity_matrix = [[1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0],
@@ -72,7 +73,7 @@ def greedyInvariantOptimizerTreeChangeFailTest_2():
     new_statistics = {StatisticsTypes.ARRIVAL_RATES: new_arrival_rates,
                       StatisticsTypes.SELECTIVITY_MATRIX: new_selectivity_matrix}
 
-    is_changed = optimizer_test(old_statistics, new_statistics, optimizer)
+    is_changed = optimizer_test(old_statistics, new_statistics, optimizer_parameters)
     result = "Succeeded" if not is_changed else "Failed"
     print(f"Test greedyInvariantOptimizerTreeChangeFailTest_2 result: {result}")
 
@@ -85,7 +86,6 @@ def greedyInvariantOptimizerTreeChangeTest_1():
     The optimizer will say that need to re-optimize
     """
     optimizer_parameters = DEFAULT_TESTING_GREEDY_INVARIANT_OPTIMIZER_SETTINGS
-    optimizer = OptimizerFactory.build_optimizer(optimizer_parameters)
 
     old_arrival_rates = [1, 5, 8, 2]
     old_selectivity_matrix = [[1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0],
@@ -99,7 +99,7 @@ def greedyInvariantOptimizerTreeChangeTest_1():
     new_statistics = {StatisticsTypes.ARRIVAL_RATES: new_arrival_rates,
                       StatisticsTypes.SELECTIVITY_MATRIX: new_selectivity_matrix}
 
-    is_changed = optimizer_test(old_statistics, new_statistics, optimizer)
+    is_changed = optimizer_test(old_statistics, new_statistics, optimizer_parameters)
     result = "Failed" if not is_changed else "Succeeded"
     print(f"Test greedyInvariantOptimizerTreeChangeTest_1 result: {result}")
 
@@ -112,7 +112,6 @@ def zstreamInvariantOptimizerTreeChangeFailTest_1():
     The optimizer will say that there is no need to re-optimize
     """
     optimizer_parameters = DEFAULT_TESTING_ZSTREAM_INVARIANT_OPTIMIZER_SETTINGS
-    optimizer = OptimizerFactory.build_optimizer(optimizer_parameters)
 
     old_arrival_rates = [5, 1, 8, 2]
     old_selectivity_matrix = [[1.0, 0.9457796098355941, 1.0, 1.0], [0.9457796098355941, 1.0, 0.15989723367389616, 1.0],
@@ -125,7 +124,7 @@ def zstreamInvariantOptimizerTreeChangeFailTest_1():
     new_statistics = {StatisticsTypes.ARRIVAL_RATES: new_arrival_rates,
                       StatisticsTypes.SELECTIVITY_MATRIX: new_selectivity_matrix}
 
-    is_changed = optimizer_test(old_statistics, new_statistics, optimizer)
+    is_changed = optimizer_test(old_statistics, new_statistics, optimizer_parameters)
     result = "Succeeded" if not is_changed else "Failed"
     print(f"Test zstreamInvariantOptimizerTreeChangeFailTest_1 result: {result}")
 
@@ -138,7 +137,6 @@ def zstreamInvariantOptimizerTreeChangeTest_1():
     The optimizer will say that need to re-optimize
     """
     optimizer_parameters = DEFAULT_TESTING_ZSTREAM_INVARIANT_OPTIMIZER_SETTINGS
-    optimizer = OptimizerFactory.build_optimizer(optimizer_parameters)
 
     old_arrival_rates = [8, 5, 1, 2, 3]
     old_selectivity_matrix = [[1.0, 1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0, 1.0],
@@ -152,7 +150,7 @@ def zstreamInvariantOptimizerTreeChangeTest_1():
     new_statistics = {StatisticsTypes.ARRIVAL_RATES: new_arrival_rates,
                       StatisticsTypes.SELECTIVITY_MATRIX: new_selectivity_matrix}
 
-    is_changed = optimizer_test(old_statistics, new_statistics, optimizer)
+    is_changed = optimizer_test(old_statistics, new_statistics, optimizer_parameters)
     result = "Succeeded" if is_changed else "Failed"
     print(f"Test zstreamInvariantOptimizerTreeChangeTest_1 result: {result}")
 
@@ -165,7 +163,6 @@ def zstreamInvariantOptimizerTreeChangeTest_2():
     The optimizer will say that need to re-optimize
     """
     optimizer_parameters = DEFAULT_TESTING_ZSTREAM_INVARIANT_OPTIMIZER_SETTINGS
-    optimizer = OptimizerFactory.build_optimizer(optimizer_parameters)
 
     old_arrival_rates = [8, 5, 1, 2, 3]
     old_selectivity_matrix = [[1.0, 1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0, 1.0],
@@ -179,23 +176,29 @@ def zstreamInvariantOptimizerTreeChangeTest_2():
     new_statistics = {StatisticsTypes.ARRIVAL_RATES: new_arrival_rates,
                       StatisticsTypes.SELECTIVITY_MATRIX: new_selectivity_matrix}
 
-    is_changed = optimizer_test(old_statistics, new_statistics, optimizer)
+    is_changed = optimizer_test(old_statistics, new_statistics, optimizer_parameters)
     result = "Succeeded" if is_changed else "Failed"
     print(f"Test zstreamInvariantOptimizerTreeChangeTest_2 result: {result}")
 
 
-def optimizer_test(old_statistics: Dict, new_statistics: Dict, optimizer: Optimizer):
+def optimizer_test(old_statistics: Dict, new_statistics: Dict, optimizer_parameters: OptimizerParameters):
     """
     Given old_statistics and new_statistics, the Optimizer decide if need to re-optimize.
     If so, return True, else return False
     """
-    pattern = get_pattern_test()
+    pattern = [get_pattern_test()]
+    statistics_collector_parameters = DEFAULT_TESTING_STATISTICS_COLLECTOR_SELECTIVITY_AND_ARRIVAL_RATES_STATISTICS
+    statistics_collector = StatisticsCollectorFactory.build_statistics_collector(statistics_collector_parameters,
+                                                                                 pattern)
+    optimizer = OptimizerFactory.build_optimizer(optimizer_parameters, statistics_collector, pattern)
 
-    if optimizer.should_optimize(old_statistics, pattern):
-        optimizer.build_new_plan(old_statistics, pattern)
+    statistics_collector.get_specific_statistics = MagicMock(return_value=old_statistics)
+    optimizer.try_optimize()
 
-    if optimizer.should_optimize(new_statistics, pattern):
-        optimizer.build_new_plan(new_statistics, pattern)
+    statistics_collector.get_specific_statistics = MagicMock(return_value=new_statistics)
+    new_tree_plan = optimizer.try_optimize()
+
+    if new_tree_plan:
         return True
 
     return False
